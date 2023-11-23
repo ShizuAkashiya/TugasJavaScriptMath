4940ece2a4f41e46152547d0a2ba102971de3c98
ShizuAkashiya <73358387+ShizuAkashiya@users.noreply.github.com>
2023-11-23 09:37:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Math.round
+let round1 = Math.round(2.84);
+console.log(round1);
+
+let round2 = Math.round(98.5423);
+console.log(round2);
+
+let round3 = Math.round(89034.345);
+console.log(round3);
+
+// Math.floor
+let floor1 = Math.floor(9.9);
+console.log(floor1);
+
+let floor2 = Math.floor(128.123);
+console.log(floor2);
+
+let floor3 = Math.floor(1532.32423);
+console.log(floor3);
+
+// Math.ceil
+let ceil1 = Math.ceil(2.84094);
+console.log(ceil1);
+
+let ceil2 = Math.ceil(934.92423);
+console.log(ceil2);
+
+let ceil3 = Math.ceil(23423.2342232);
+console.log(ceil3);
+
+// Math.pow
+let pow1 = Math.pow(31, 3);
+console.log(pow1);
+
+let pow2 = Math.pow(45, 6);
+console.log(pow2);
+
+let pow3 = Math.pow(60, 9);
+console.log(pow3);
+
+// Math.sqrt
+let sqrt1 = Math.sqrt(36);
+console.log(sqrt1);
+
+let sqrt2 = Math.sqrt(64);
+console.log(sqrt2);
+
+let sqrt3 = Math.sqrt(81);
+console.log(sqrt3);
